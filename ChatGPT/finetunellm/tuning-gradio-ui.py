38b1c111bbe8a68840a21bc5be51595c10b561de
Avkash Chahuan <avkashchahuan@Avkashs-MacBook-Pro-3.local>
2023-03-19 09:50:16 -0700

    gradio ui llm fine tuning added

--- ChatGPT/finetunellm/tuning-gradio-ui.py
+++ ChatGPT/finetunellm/tuning-gradio-ui.py
@@ -1,66 +1,3 @@
-# Finetuning LLM # 
-LLM can be  extended, by fine tuning with custom dataset to provide Q&A, summary and many other ChatGPT like functions.  In this video, we are using LlamaIndex (GPT Index) which provides a central interface to connect your LLM's with external data. 
-
-### Using LlamaIndex (previously known as GPT-Index)
-LlamaIndex (GPT Index) is a project that provides a central interface to connect your LLM's with external data. 
-
-- https://github.com/jerryjliu/llama_index
-- https://llamahub.ai/
-- https://gpt-index.readthedocs.io/en/latest/reference/readers.html
-
-
-### Video Tutorial - Fine Tuning LLM with Custom Data (using Open AI GPT) in python
-<table class="table table-striped table-bordered table-vcenter">
-    <tr>
-        <td align="center"><b>🔥&nbsp;Fine Tuning LLM with Custom Data (using Open AI GPT) in python</b></td>
-    </tr>
-    <tr>
-        <td>
-            <div>
-                
-[![Fine Tuning LLM with Custom Data (using Open AI GPT) in python](https://img.youtube.com/vi/6aOzoJKNLKQ/0.jpg)](https://www.youtube.com/watch?v=6aOzoJKNLKQ)
-
- </tr>
-</table>
-
-### finetune.py
-```
-import os
-os.environ["OPENAI_API_KEY"] = 'sk-your_key'
-
-from gpt_index import GPTSimpleVectorIndex, SimpleDirectoryReader
-documents = SimpleDirectoryReader('data').load_data()
-index = GPTSimpleVectorIndex(documents)
-
-# save to disk
-index.save_to_disk('index.json')
-
-
-```
-
-### Query with Index - queryindex.py
-```
-import os
-os.environ["OPENAI_API_KEY"] = 'sk-your_key'
-
-from gpt_index import GPTSimpleVectorIndex
-
-# load from disk
-index = GPTSimpleVectorIndex.load_from_disk('indexes/index.json')
-
-statement = "start"
-
-while statement != 'exit':
-    statement = input('What is your question? > ')
-    print(" Your question is - " + statement)
-    print("Answer: ")
-    print(index.query(statement))
-
-```
-
-
-### Gradio UI Code - turning-gradio-ui.py
-```
 import gradio as gr
 import os
 from gpt_index import SimpleDirectoryReader, GPTSimpleVectorIndex
@@ -246,5 +183,3 @@ if __name__ == '__main__':
     gradio_ui = GPTProcessing(my_app)
     gradio_ui.create_ui()
     gradio_ui.launch_ui()
-
-```
